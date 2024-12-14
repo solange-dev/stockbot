a86d3f7cab6176c4ce033756a50c37a99fdf4281
Sakal1009 <verasdone6@gmail.com>
2024-12-14 18:21:25 +0900

    favicon change

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata = {
   description:
     'Lightning Fast AI Chatbot that Responds With Live Interactive Stock Charts, Financials, News, Screeners, and More.',
   icons: {
-    icon: '/favicon.ico',
+    icon: '/favicon.png',
     shortcut: '/favicon-16x16.png',
     apple: '/apple-touch-icon.png'
   }
