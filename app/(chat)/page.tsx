a315881306477587d6f1a4e7e7ac98e9cc0a21b5
Sakal1009 <verasdone6@gmail.com>
2024-12-14 18:19:52 +0900

    page metadata changed

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -5,7 +5,8 @@ import { Session } from '@/lib/types'
 import { getMissingKeys } from '@/app/actions'
 
 export const metadata = {
-  title: 'StockBot powered by Groq'
+  // title: 'StockBot powered by Groq'
+  title: 'Solange'
 }
 
 export default async function IndexPage() {
